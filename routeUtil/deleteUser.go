eb374223cddddc30ae4c8cafaa89cd575a767055
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-01-03 00:00:09 -0800

    del account

--- routeUtil/deleteUser.go
+++ routeUtil/deleteUser.go
@@ -12,19 +12,23 @@ import (
 )
 
 
-func fetchUser() gin.HandlerFunc {
+func DeleteUser() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ident := c.Param("uid")
-		var userFound storageUtil.User
+		
 
 		oID, _ := primitive.ObjectIDFromHex(ident)
 		//converting id form param from hex and assigning it to oid
 
-		err := userCol.FindOne(c, bson.M{"id": oID}).Decode(&userFound) //finding user and decoding and transferring into userfound struct
+		res, err := userCol.DeleteOne(c, bson.M{"id": oID}) //finding user and decoding and transferring into userfound struct
 
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, storageUtil.Response{Code: http.StatusInternalServerError, Message: "Internal Server Error", Success: false})
 			return
+		}
+
+		if res.DeletedCount == 0 {
+			c.JSON(http.StatusNotFound, storageUtil.Response{Code: http.StatusNotFound, Message: "Not Found", Success: false})
 		}
 
 		c.JSON(http.StatusOK, storageUtil.Response{Code: http.StatusOK, Message: "OK", Success: true})
