0abb77d3bce6cc6fc9a121b6e1c2fefd44ec8544
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-01-03 13:48:06 -0800

    roiyuted errors

--- routeUtil/userRoutes.go
+++ routeUtil/userRoutes.go
@@ -9,7 +9,7 @@ import (
 func UserRoutes(router *gin.Engine) {
 	router.GET("/users/:uid", FetchUser())
 	router.GET("/users", FetchUsers())
-	router.POST("/user", CreateUser())
+	router.POST("/users", CreateUser())
 	router.DELETE("/users/:uid", DeleteUser())
 	router.PUT("/users/:uid", EditUser())
 }
