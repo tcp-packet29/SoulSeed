28b3d1d0081ea00b65bf4e9453ba466bd504cfd8
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-01-02 21:55:34 -0800

    database handling util

---
+++ dbUtil/access.go
@@ -0,0 +1,20 @@
+package dbUtil
+
+import (
+	"main/genUtil"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+
+//single instance of mongo client so we dont createa new client t o access the server from every time
+var client *mongo.Client = genUtil.ConnectToMongo()
+
+func GetClient() *mongo.Client {
+	return client
+}
+
+func GetCollection(collectionName string) *mongo.Collection {
+	return client.Database("Cluster0").Collection(collectionName)
+}
+
---
+++ genUtil/connect.go
@@ -0,0 +1,29 @@
+package genUtil
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+func ConnectToMongo() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(getMongoURI()))
+	if err != nil {
+		fmt.Println("Error creating client")
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	err = client.Connect(ctx)
+	//err alrexists, just reassigning
+	if err != nil {
+		fmt.Println("Error connecting to client")
+	}
+
+	fmt.Println("Connected to MDB db")
+	return client
+}
+
+
+
