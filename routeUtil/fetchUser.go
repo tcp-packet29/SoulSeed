608a086785fe5fb736d786171490edd6caf4ed3a
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-01-02 23:04:32 -0800

    should probably move fuikles into one

---
+++ routeUtil/fetchUser.go
@@ -0,0 +1,31 @@
+package routeUtil
+
+import (
+	"main/dbUtil"
+	"main/storageUtil"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func fetchUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ident := c.Param("uid")
+		var userFound storageUtil.User
+
+		oID, _ := primitive.ObjectIDFromHex(ident)
+		//converting id form param from hex and assigning it to oid
+
+		err := userCol.FindOne(c, bson.M{"id": oID}).Decode(&userFound) //finding user and decoding and transferring into userfound struct
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, storageUtil.Response{Code: http.StatusInternalServerError, Message: "Internal Server Error", Success: false})
+			return
+		}
+
+		c.JSON(http.StatusOK, storageUtil.Response{Code: http.StatusOK, Message: "OK", Success: true})
+	}
+}
