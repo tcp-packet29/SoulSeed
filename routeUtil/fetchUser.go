e2a7c0ca541d07fbf789a98e48cfd3e2251594d5
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-01-29 17:36:56 -0800

    jwt finished

--- routeUtil/fetchUser.go
+++ routeUtil/fetchUser.go
@@ -44,5 +44,40 @@ func JWTGen() (string, error) {
 	claims["iat"] = time.Now() //time of creation
 	claims["user"] = "user"
 
-	tokenString, err := token.SignedString()
+	tokenString, err := token.SignedString(jwtEncryptionKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil //no error
+}
+
+func verifyJWToken( handlFunc func(c *gin.Context)) gin.HandlerFunc { //takes in reuest handlign rfunc as param to add process and middlerwar
+	return func (c *gin.Context) {
+		if c.Header["Token"] == nil {
+			c.JSON(http.StatusUnauthorized, storageUtil.Response{Code: http.StatusUnauthorized, Message: "Unauthorized (no token exists)", Success: false, Data: nil})
+			return
+		} else {
+			token, err := jwt.Parse(c.Header["Token"][0], func(tok *jwt.Token) (interface{}, error) {
+				_, valid :=token.Method.(*jwt.SigningMethodECDSA)
+				if !valid {
+					c.JSON(http.StatusUnauthorized, storageUtil.Response{Code: http.StatusUnauthorized, Message: "Unauthorized due to jwt not being from provider", Success: false, Data: nil})
+					return nil, nil
+				}
+				return "", nil
+			})
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, storageUtil.Response{Code: http.StatusUnauthorized, Message: "Unauthorized due to jwt not being parsed", Success: false, Data: nil})
+				return
+			}
+
+			if token.Valid {
+				handlFunc(c)
+			} else {
+				c.JSON(http.StatusUnauthorized, storageUtil.Response{Code: http.StatusUnauthorized, Message: "Unauthorized due to jwt not being valid due to some reason", Success: false, Data: nil})
+				return
+			}
+
+			
+		}
+	}
 }
