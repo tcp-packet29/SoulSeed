e03c437f4049a46e8d1266c915e9fff8a56559cc
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-02-16 15:56:13 -0800

    my code ocmpiled

--- routeUtil/editOrganization.go
+++ routeUtil/editOrganization.go
@@ -1,9 +1,8 @@
- package routeUtil
+package routeUtil
 
 import (
-
-	"main/storageUtil"
 	"main/genUtil"
+	"main/storageUtil"
 
 	"net/http"
 
@@ -13,7 +12,6 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-
 )
 
 func EditOrganization() gin.HandlerFunc {
@@ -25,12 +23,12 @@ func EditOrganization() gin.HandlerFunc {
 		if err != nil {
 			fmt.Println("e")
 			c.JSON(http.StatusBadRequest, storageUtil.Response{Code: http.StatusBadRequest, Message: "Body was not an organization", Success: false, Data: nil})
-			//return reponse    
+			//return reponse
 			//send a response back to the client if error
 			//return object of response struct
 		}
 
-		oID, err := primitive.ObjectIDFromHex(orgId) 
+		oID, err := primitive.ObjectIDFromHex(orgId)
 		if err != nil {
 			fmt.Println("e")
 			//return response
@@ -38,8 +36,6 @@ func EditOrganization() gin.HandlerFunc {
 		fmt.Print(oID.Hex())
 		fmt.Print(orgId)
 		//will print the same thing
-
-
 
 	}
 }
@@ -59,24 +55,26 @@ func AddUserOrganization() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, storageUtil.Response{Code: http.StatusBadRequest, Message: "Bad Request", Success: false, Data: nil})
 			return
 		}
-		organization := genUtil.FetchOrgById(uid, OrganizationCol, c, func() {c.JSON(http.StatusNotFound, storageUtil.Response{Code: http.StatusNotFound, Message: "Not Found", Success: false, Data: nil})
-			
+		organization := genUtil.FetchOrgById(uid, OrganizationCol, c, func() {
+			c.JSON(http.StatusNotFound, storageUtil.Response{Code: http.StatusNotFound, Message: "Not Found", Success: false, Data: nil})
+
 		})
 
 		userList := organization.Users_ID
 		userList = append(userList, addedUser.Id.Hex())
 
 		finalOrganization := storageUtil.Organization{
-			Id: organization.Id,
-			Name: organization.Name,
+			//new orgnaizaiton odel
+			Id:          organization.Id,
+			Name:        organization.Name,
 			Description: organization.Description,
-			Image: organization.Image,
-			Zipcode: organization.Zipcode,
-			Owner: organization.Owner,
-			Owner_ID: organization.Owner_ID,
-			Items: organization.Items,
-			Users: append(organization.Users, addedUser), //or could jsut add user by idas9iofg or just add hrt eentire user
-			Users_ID: userList,
+			Image:       organization.Image,
+			Zipcode:     organization.Zipcode,
+			Owner:       organization.Owner,
+			Owner_ID:    organization.Owner_ID,
+			Items:       organization.Items,
+			Users:       append(organization.Users, addedUser), //or could jsut add user by idas9iofg or just add hrt eentire user
+			Users_ID:    userList,
 		}
 
 		result, err := OrganizationCol.UpdateOne(c, bson.M{"id": uid}, bson.M{"$set": finalOrganization})
@@ -86,8 +84,6 @@ func AddUserOrganization() gin.HandlerFunc {
 		}
 
 		c.JSON(http.StatusOK, storageUtil.Response{Code: http.StatusOK, Message: "OK", Success: true, Data: map[string]interface{}{"data": result}})
-
-
 
 	}
 }
@@ -102,23 +98,25 @@ func AddItemOrganization() gin.HandlerFunc {
 			return
 		}
 
-		organization := genUtil.FetchOrgById(uid, OrganizationCol, c, func() {c.JSON(http.StatusNotFound, storageUtil.Response{Code: http.StatusNotFound, Message: "Not Found", Success: false, Data: nil})})
+		organization := genUtil.FetchOrgById(uid, OrganizationCol, c, func() {
+			c.JSON(http.StatusNotFound, storageUtil.Response{Code: http.StatusNotFound, Message: "Not Found", Success: false, Data: nil})
+		})
 		//finding organization
 
 		itemList := organization.Items
 		itemList = append(itemList, addedItem) //no list of item ids since too cmplicated and items rae smaller just have lis of titme structs
 
 		finalOrganization := storageUtil.Organization{
-			Id: organization.Id,
-			Name: organization.Name,
+			Id:          organization.Id,
+			Name:        organization.Name,
 			Description: organization.Description,
-			Image: organization.Image,
-			Zipcode: organization.Zipcode,
-			Owner: organization.Owner,
-			Owner_ID: organization.Owner_ID,
-			Items: itemList,
-			Users: organization.Users, //or could jsut add user by idas9iofg
-			Users_ID: organization.Users_ID,
+			Image:       organization.Image,
+			Zipcode:     organization.Zipcode,
+			Owner:       organization.Owner,
+			Owner_ID:    organization.Owner_ID,
+			Items:       itemList,
+			Users:       organization.Users, //or could jsut add user by idas9iofg
+			Users_ID:    organization.Users_ID,
 		}
 
 		result, err := OrganizationCol.UpdateOne(c, bson.M{"id": uid}, bson.M{"$set": finalOrganization})
@@ -129,7 +127,9 @@ func AddItemOrganization() gin.HandlerFunc {
 
 		c.JSON(http.StatusOK, storageUtil.Response{Code: http.StatusOK, Message: "OK", Success: true, Data: map[string]interface{}{"data": result}})
 
-
-
 	}
+}
+
+func startTimeout() {
+
 }
