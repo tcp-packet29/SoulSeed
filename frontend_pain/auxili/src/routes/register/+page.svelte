882a28922c57924743a48186d3a859080544ca79
Gaurav Bansal <bansal22.gaurav@gmail.com>
2023-02-19 20:39:56 -0800

    toks

--- frontend_pain/auxili/src/routes/register/+page.svelte
+++ frontend_pain/auxili/src/routes/register/+page.svelte
@@ -7,12 +7,14 @@
     let confirm = "";
     let zipcode = "";
     let email= "";
+    let code ="";
 
     let valid= false;
 
     function fillIn(one, two, btn) {
         document.getElementById("textone").innerHTML = one;
         document.getElementById("texttwo").innerHTML = two;
+        //mycodehagthcomepidl
         document.getElementById("close").innerHTML = btn;
     } //decode then weas not assigned or issued by this server mthe provider
 
@@ -22,8 +24,45 @@
     //in addition implement profile api for processing request and sending response
     //i rember i should make my ownr fabitmq     clone and rep it thrugh a tui using golang tui library called tvuiew e vcayuse then u can represetn queures through trui using tview
 
+    let tok = "";
+
     function leadToLogin() {
         if (valid) {
+            axios.get("http://localhost:8080/email/confirm/" + email)
+                .then(function (response) {
+                    let jsooo = JSON.parse(JSON.stringify(response.data))
+                    console.log(jsooo)
+                    tok = jsooo.data.Token.toString();
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+
+    function create() {
+        if tok === code {
+            axios.post('http://localhost:8080/access/users', {
+                "username": uname,
+                "password": pword,
+                "email": email,
+                "zipcode": zipcode,
+            })
+                .then(function (response) {
+                    if (response.status == 201 || response.status == 200) {
+                        valid = true;
+                        fillIn("Final Step!", "A registration code was emailed to you (check your spam folder)", "Continue");
+
+                    } else {
+                        fillIn("Error", "Username already exists", "Close");
+                    }
+                    console.log(response);
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
             axios.post('http://localhost:8080/access/login', {
                 "username": uname,
                 "password": pword
@@ -40,6 +79,7 @@
                     //idempotenmt
                     if (browser) {
                         window.localStorage.setItem("token", jso.Token.toString());
+                        console.log(window.localStorage.getItem("token"))
                     }
 
                     console.log(window.localStorage.getItem("token"))
@@ -48,28 +88,6 @@
                 .catch(function (error) {
                     console.log(error);
                 });
-            uname = "";
-            pword = "";
-            confirm = "";
-            zipcode = "";
-
-            axios.get("http://localhost:8080/email/confirm/" + email)
-                .then(function (response) {
-                    let jsooo = JSON.parse(JSON.stringify(response.data))
-                    console.log(jsooo)
-                    if (browser) {
-                        window.localStorage.setItem("conf", jsooo.data.Token.toString());
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-
-            window.location.href = "http://localhost:5173/confirm";
-
-
-
-
         }
     }
     //checking zipcode on frontend, could do on bacvkend
@@ -83,39 +101,16 @@
 
 
 
-
-    
-    function createUser() {
+    function check() {
         if (uname.trim() == "" || pword.trim() == "" || zipcode.trim() == "" || confirm.trim() == "") {
             fillIn("Error", "Please fill in all fields", "Close");
-            return;
         } else if (pword != confirm) {
             fillIn("Error", "Passwords do not match", "Close");
-            return;
+
         } else if (!validZipcode(zipcode)) {
             fillIn("Error", "Zipcode is not valid", "Close");
-            return;
+
         }
-        axios.post('http://localhost:8080/access/users', {
-            "username": uname,
-            "password": pword,
-            "email": email,
-            "zipcode": zipcode,
-        })
-        .then(function (response) {
-            if (response.status == 201 || response.status == 200) {
-                valid= true;
-                fillIn("Account Created!", "You can now login to your account.", "Login!");
-                
-            } else {
-                fillIn("Error", "Username already exists", "Close");
-            }
-            console.log(response);
-            
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
     }
 
 </script>
@@ -142,52 +137,65 @@
     <h2><a class="btn btn-ghost btn-sm rounded-btn bg-secondary" href="http://localhost:5173/login">Login</a></h2>
 
 </div>
+
 <div class="flex h-screen justify-center items-center">
-    <div data-theme="mycodecompiled" class="card w-96 bg-secondary bg-base-200 text-neutral-content shadow-xl">
-        <div class="card-body items-center text-center">
-            <h2 class="card-title text-neutral">Register</h2>
-            <p class="card-subtitle text-primary">An app for vendors and customers; with emphasis on flexibility.</p>
-            <div class="form-control">
-                <label class="label">
-                    <span class="label-text">Username</span>
+    <div>
+        <div data-theme="mycodecompiled" class="card w-96 bg-secondary bg-base-200 text-neutral-content shadow-xl">
+            <div class="card-body items-center text-center">
+                <h2 class="card-title text-neutral">Register</h2>
+                <p class="card-subtitle text-primary">An app for vendors and customers; with emphasis on flexibility.</p>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Username</span>
 
-                </label>
-            
-                    <input type="text" placeholder="myCodeCompiled42" class="input input-bordered input-accent w-full max-w-xs" bind:value={uname}/>
+                    </label>
 
-                <label class="label">
-                    <span class="label-text">Email</span>
-                </label>
+                        <input type="text" placeholder="myCodeCompiled42" class="input input-bordered input-accent w-full max-w-xs" bind:value={uname}/>
 
-                <input type="text" placeholder="golang@golang.go" class="input input-bordered input-accent w-full max-w-xs" bind:value={email}/>
-                
-                <label class="label">
-                    <span class="label-text">Password</span>
-                </label>
-                
-                    <input type="password" placeholder="clojureIsBestJVMLang" class="input input-bordered input-accent w-full max-w-xs" bind:value={pword}/>
-                    
-                
-                <label class="label">
-                    <span class="label-text">Confirm Password</span>
-                </label>
-                
-                    <input type="password" placeholder="clojureIsBestJVMLang" class="input input-bordered input-accent w-full max-w-xs" bind:value={confirm}/>
-                
-                <label class="label">
-                    <span class="label-text">Zipcode</span>
-                </label>
-                
-                    <input type="text" placeholder="11111" class="input input-bordered input-accent w-full max-w-xs" bind:value={zipcode}/>
-                
+                    <label class="label">
+                        <span class="label-text">Email</span>
+                    </label>
+
+                    <input type="text" placeholder="golang@golang.go" class="input input-bordered input-accent w-full max-w-xs" bind:value={email}/>
+
+                    <label class="label">
+                        <span class="label-text">Password</span>
+                    </label>
+
+                        <input type="password" placeholder="clojureIsBestJVMLang" class="input input-bordered input-accent w-full max-w-xs" bind:value={pword}/>
+
+
+                    <label class="label">
+                        <span class="label-text">Confirm Password</span>
+                    </label>
+
+                        <input type="password" placeholder="clojureIsBestJVMLang" class="input input-bordered input-accent w-full max-w-xs" bind:value={confirm}/>
+
+                    <label class="label">
+                        <span class="label-text">Zipcode</span>
+                    </label>
+
+                        <input type="text" placeholder="11111" class="input input-bordered input-accent w-full max-w-xs" bind:value={zipcode}/>
+
+                </div>
+                <div class="card-actions">
+                    <label for="donemodal" class="btn btn-outline btn-accent" on:click={check}>Register</label>
+                </div>
             </div>
-            
-            
-           -= 
-           
-            <div class="card-actions">
-                <label for="donemodal" class="btn btn-outline btn-accent" on:click={createUser}>Register</label>
+
+            <div class="divider text-accent"> & </div>
+
+            <div data-theme="mycodecompiled" class="card w-96 bg-secondary bg-base-200 text-neutral-content shadow-xl">
+                <div class="card-body items-center text-center">
+                    <h2 class="card-title text-neutral">Confirmation</h2>
+                    <p class="mb-5">Please enter your confirmation code that was sent to your email.</p>
+                    <input type="password" placeholder="Password" class="input input-bordered input-accent w-full max-w-xs" bind:value={code}/>
+                    <div class="card-actions">
+                        <button class="btn btn-outline btn-accent" on:click={create}>Check</button>
+                    </div>
+                </div>
             </div>
+
         </div>
     </div>
 </div>
